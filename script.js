74cb13b60dd1df312d33b4558f31ee660099f25f
Staubtornado <76395253+Staubtornado@users.noreply.github.com>
2022-09-13 20:35:19 +0200

    Moved connection code to content.js

--- script.js
+++ script.js
@@ -1,18 +1,7 @@
 const {app, BrowserWindow, globalShortcut} = require("electron");
 
 app.whenReady().then(() => {
-    globalShortcut.register('MediaPlayPause', () => {
-        const net = require('net');
-
-        const client = new net.Socket();
-        client.connect({port: 65432, host: "164.92.185.110"}, () => {
-            client.write("Hello World\r");
-        });
-        client.on('data', (data) => {
-            console.log(data.toString('utf-8'));
-            client.end(() => {});
-        });
-    });
+    globalShortcut.register('MediaPlayPause', () => {});
 }).then(createWindow);
 
 function createWindow() {
