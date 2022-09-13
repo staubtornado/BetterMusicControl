eafd55612e02b32dec46971f9ec346c6b06b00df
Staubtornado <76395253+Staubtornado@users.noreply.github.com>
2022-09-13 16:00:00 +0200

    Added socket data send & receive

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ const {app, BrowserWindow, globalShortcut} = require("electron");
 
 app.whenReady().then(() => {
     globalShortcut.register('MediaPlayPause', () => {
-        console.log("Worked")
+        const net = require('net');
+
+        const client = new net.Socket();
+        client.connect({port: 65432, host: "164.92.185.110"}, () => {
+            client.write("Hello World\r");
+        });
+        client.on('data', (data) => {
+            console.log(data.toString('utf-8'));
+            client.end(() => {});
+        });
     });
 }).then(createWindow);
 
